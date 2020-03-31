cdaa2ae018c8813d9c19441e121b95348b0a97b9
sapinderbhullar <simmi08979@gmail.com>
2020-03-31 22:06:25 +0000

    re-organising

---
+++ private/app/controllers/main.php
@@ -0,0 +1,31 @@
+<?php
+
+class Main extends Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+    
+    /*
+     * http://localhost/
+     */
+    function Index () {
+
+        $this->view("template/header");
+        $this->view("main/index");
+        $this->view("template/footer");
+        
+    }
+
+    function Other () {
+
+        $this->view("template/header");
+        $this->view("main/other");
+        echo("<br><br><br>hello there");
+        $this->view("template/footer");
+        
+    }
+
+}
+
+?>
