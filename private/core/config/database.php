00ad41d1f38fffd75d351214b1085e6ea737a4b4
sapinderbhullar <simmi08979@gmail.com>
2020-04-08 18:57:14 +0000

    reorganising

--- private/core/config/database.php
+++ private/core/config/database.php
@@ -1,9 +1,9 @@
 <?php
     
     $this->config["database"]["driver"] = "mysql";
-    $this->config["database"]["dbhost"] = "";
-    $this->config["database"]["dbname"] = "";
-    $this->config["database"]["username"] = "baritchie";
-    $this->config["database"]["password"] = "";
+    $this->config["database"]["dbhost"] = "db";
+    $this->config["database"]["dbname"] = "CMS";
+    $this->config["database"]["username"] = "sapinder";
+    $this->config["database"]["password"] = "password";
 
 ?>
